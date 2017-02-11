fb516cc7885a12b090d53e836388b0b60cfb9904
gayanjeewantha2@gmail.com <gayanjeewantha2@gmail.com>
2017-02-11 16:52:25 +0530

    next version

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("This is the edited version");
+		System.out.println("this is the edited version");
 	}
 }
